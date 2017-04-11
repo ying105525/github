27d20b4b83c087ae3f9561ed10bf759ba3a61a6e
ying105525 <420137138@qq.com>
2017-04-11 22:04:24 +0800

    windows1

---
+++ src/IteratorEach.java
@@ -0,0 +1,17 @@
+import java.util.Collection;
+import java.util.HashSet;
+
+public class IteratorEach {
+	public static void main(String args[]){
+		Collection books = new HashSet();
+		books.add("«·¡øº∂JAVA EE");
+		books.add("fengkuang java");
+		
+		
+		
+	}
+	
+	
+	
+	
+}
